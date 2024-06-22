01741fb96587b0f13baedd5bca9dbd50b90098b7
Suvetha S <131991850+Suvetha1111@users.noreply.github.com>
2024-06-22 13:32:52 +0530

    Create FindAllMiss.java

---
+++ Arrays/FindAllMiss.java
@@ -0,0 +1,52 @@
+/*
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ Using Cyclic sort
+*/
+class Solution{
+   
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // just find missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index+1) {
+                ans.add(index + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        List<Integer> ans = findDisappearedNumbers(arr);
+        System.out.println(ans);
+    }
+    }
