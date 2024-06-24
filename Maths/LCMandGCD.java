80d7781b00407fc6b19d11a22307b35747a8806a
Suvetha S <131991850+Suvetha1111@users.noreply.github.com>
2024-06-24 13:45:03 +0530

    Update LCMandGCD.java

--- Maths/LCMandGCD.java
+++ Maths/LCMandGCD.java
@@ -1,4 +1,8 @@
-public class GCD_LCM {
+/*
+The highest number which divides 20 and 30 is 10. So the GCD of 20, 30 is 10.
+The lowest number that can be divided by 20 and 30, leaving remainder 0 is 60.
+So the LCM of 20 and 30 is 60.
+*/public class GCD_LCM {
     public static void main(String[] args) {
 //        System.out.println(gcd(4, 9));
         System.out.println(lcm(2, 7));
