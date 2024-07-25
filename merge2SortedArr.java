8f101359ea0aa29d579a8d9cc3dbc102578e9ae2
Suvetha S <131991850+Suvetha1111@users.noreply.github.com>
2024-07-25 21:39:46 +0530

    Update merge2SortedArr.java

--- merge2SortedArr.java
+++ merge2SortedArr.java
@@ -10,26 +10,20 @@ Merged array: 2,3,4,5,6,7,8,9,10,11,13,15
 
 
 import java.util.Scanner;
-      public class merge2sortedArr
+public class merge2sortedArr
 {
     public static void main(String artgs[])
     {
         Scanner in = new Scanner(System.in);
-        
         int[] array1 = {2,4,5,6,7,9,10,13};//write as getting input from user 
         int[] array2 = {2,3,4,5,6,7,8,9,11,15};
-        
         int[] array3 = new int[array1.length+array2.length];
-        
         for (int i=0; i<array1.length; i++)
             array3[i] = array1[i];
-        
         for (int i=array1.length,j=0; i<array1.length+array2.length; i++,j++)
             array3[i] = array2[j];
-        
     //    for(int i=0; i<array3.length; i++)
     //      System.out.print(array3[i]+ " ");
-        
         for(int i=0; i<array3.length; i++)
             for(int j=i+1; j<array3.length; j++)
             if(array3[i] > array3[j])
@@ -40,7 +34,6 @@ import java.util.Scanner;
                 }
       //for(int i=0; i<array3.length; i++)
       //    System.out.print(array3[i]+ " ");
-        
         System.out.println(array3[0]);
         for(int i=1; i<array3.length; i++)
         {
